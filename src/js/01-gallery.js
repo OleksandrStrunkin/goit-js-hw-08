b157c964a5f406af77ee806335ba4259a1b90878
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2022-11-19 18:04:04 +0200

    1 work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,25 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+
+const pallet = document.querySelector(`.gallery`);
+const boxMarkup = createImgBox(galleryItems);
+
+pallet.insertAdjacentHTML('beforeend', boxMarkup)
+
+function createImgBox(boxes) {
+    return boxes.map(({ preview, original, description }) => {
+        return  `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`
+    }).join(``);
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: `alt`,
+    captionDelay: 250,
+});
