a5296c2df89c58b45ebc8a993c5c19ed3e15ac96
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2022-11-20 01:21:40 +0200

    work2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+ player.on('timeupdate', throttle(onPlayUpdate, 1000));
+function onPlayUpdate(e) {
+    localStorage.setItem(STORAGE_KEY, e.seconds);
+}
+
+
+function onTime(t) {
+    const dataTime = localStorage.getItem(STORAGE_KEY)
+    if (dataTime) {
+        player.setCurrentTime(dataTime)
+    }  
+}
+ 
+onTime();
